67c8b176c96585d79f66f4af1fec9c2a5567460d
Manaure Vasconcelos <manaurevasc97@gmail.com>
2024-04-26 16:44:16 -0300

    types variables

---
+++ src/edu/manaure/javaInitial/MyClass.java
@@ -0,0 +1,20 @@
+package edu.manaure.javaInitial;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MyClass {
+    // Metodo principal da classe.
+    public static void main(String[] args) {
+        //class.saida.printarNaTela(algo) => a class System tem varios comandos de saida de entrada.
+        //System.out.println("Hello word");
+        String first_name = "Manaure";
+        String last_name = "Vasconcelos";
+
+        String fullName = fullName(first_name, last_name);
+        System.out.println(fullName);
+    }
+
+    public static String fullName (String first_name, String last_name) {
+       return first_name.concat(" ").concat(last_name);
+    }
+}
---
+++ src/edu/manaure/javaInitial/TypesVariable.java
@@ -0,0 +1,17 @@
+package edu.manaure.javaInitial;
+
+public class TypesVariable {
+    // int, byte, short, long, float, double, boolean, char
+    int year = 2024; // inteiro e pode ser mutavel.
+    byte age = 26; // -128 <> 127
+    long cpf = 000000000000;
+    float pi = 3.14F; // Finaliza com F
+    boolean logged = true;
+    double salary = 1000.20;
+
+    final String BR = "Brasil"; // letras maiusculas e "final" para uma constante
+    //BR = "Brazil";
+
+    String name = "Manaure"; // class string que representa textos
+
+}
